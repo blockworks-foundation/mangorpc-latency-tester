b0edf8bd10d532603cba719b3020d2fa60591d33
GroovieGermanikus <groovie@mango.markets>
2024-06-03 18:58:13 +0200

    add slots from geyser

--- src/slot_latency_tester.rs
+++ src/slot_latency_tester.rs
@@ -40,16 +40,6 @@ async fn main() {
 
     let grpc_addr = std::env::var("GRPC_ADDR").unwrap();
 
-    rpc_gpa(&rpc_client).await;
-
-    rpc_get_account_info(&rpc_client).await;
-
-    rpc_get_token_accounts_by_owner(&rpc_client).await;
-
-    rpc_get_signatures_for_address(&rpc_client).await;
-
-    websocket_account_subscribe(Url::parse(ws_url2.as_str()).unwrap()).await;
-
     let (slots_tx, mut slots_rx) = tokio::sync::mpsc::channel(100);
 
     let timeouts = GrpcConnectionTimeouts {
@@ -62,9 +52,7 @@ async fn main() {
 
     let config = GrpcSourceConfig::new(grpc_addr.to_string(), None, None, timeouts.clone());
 
-    start_geyser_all_accounts_task(config.clone());
-
-    start_geyser_orca_token_account_task(config.clone());
+    start_geyser_slots_task(config.clone(), slots_tx.clone());
 
     tokio::spawn(websocket_source(Url::parse(ws_url1.as_str()).unwrap(), slots_tx.clone()));
     tokio::spawn(websocket_source(Url::parse(ws_url2.as_str()).unwrap(), slots_tx.clone()));
@@ -81,159 +69,6 @@ async fn main() {
     }
 
    sleep(Duration::from_secs(15));
-}
-
-
-// note: this might fail if the yellowstone plugin does not allow "any broadcast filter"
-fn start_geyser_all_accounts_task(config: GrpcSourceConfig) {
-    let green_stream = create_geyser_reconnecting_stream(
-        config.clone(),
-        // orca_token_account(),
-        all_accounts(),
-    );
-
-    tokio::spawn(async move {
-        let mut green_stream = pin!(green_stream);
-        while let Some(message) = green_stream.next().await {
-            match message {
-                Message::GeyserSubscribeUpdate(subscriber_update) => {
-                    match subscriber_update.update_oneof {
-                        Some(UpdateOneof::Account(update)) => {
-                            info!("ORCA Account: {:?}", update.account.unwrap().pubkey);
-                        }
-                        _ => {}
-                    }
-                }
-                _ => {}
-            }
-        }
-    });
-}
-
-fn start_geyser_orca_token_account_task(config: GrpcSourceConfig) {
-    let green_stream = create_geyser_reconnecting_stream(
-        config.clone(),
-        orca_token_account(),
-    );
-
-    tokio::spawn(async move {
-        let mut green_stream = pin!(green_stream);
-        while let Some(message) = green_stream.next().await {
-            match message {
-                Message::GeyserSubscribeUpdate(subscriber_update) => {
-                    match subscriber_update.update_oneof {
-                        Some(UpdateOneof::Account(update)) => {
-                            info!("ORCA Account: {:?}", update.account.unwrap().pubkey);
-                        }
-                        _ => {}
-                    }
-                }
-                _ => {}
-            }
-        }
-    });
-}
-
-async fn rpc_gpa(rpc_client: &RpcClient)  {
-
-    // TODO choose a smaller program
-    // 4MangoMjqJ2firMokCjjGgoK8d4MXcrgL7XJaL3w6fVg
-    let program_pubkey = Pubkey::from_str("CPLT8dWFQ1VH4ZJkvqSrLLFFPtCcKDm4XJ51t4K4mEiN").unwrap();
-
-    // tokio::time::sleep(Duration::from_millis(100)).await;
-    let program_accounts = rpc_client
-        .get_program_accounts(&program_pubkey)
-        .await
-        .unwrap();
-
-    info!("Program accounts: {:?}", program_accounts.len());
-    // mango 12400 on mainnet
-    // CPL: 107 on mainnet
-
-}
-
-async fn rpc_get_account_info(rpc_client: &RpcClient) {
-    let program_pubkey = Pubkey::from_str("4MangoMjqJ2firMokCjjGgoK8d4MXcrgL7XJaL3w6fVg").unwrap();
-
-    let account_info = rpc_client
-        .get_account(&program_pubkey)
-        .await
-        .unwrap();
-
-    info!("Account info: {:?}", account_info);
-
-}
-
-async fn rpc_get_token_accounts_by_owner(rpc_client: &RpcClient) {
-    let owner_pubkey = Pubkey::from_str("SCbotdTZN5Vu9h4PgSAFoJozrALn2t5qMVdjyBuqu2c").unwrap();
-    let mint = Pubkey::from_str("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v").unwrap();
-
-    let token_accounts = rpc_client
-        .get_token_accounts_by_owner(
-            &owner_pubkey,
-            TokenAccountsFilter::Mint(mint),
-        )
-        .await
-        .unwrap();
-
-    // 1 account
-    info!("Token accounts: {:?}", token_accounts.len());
-}
-
-async fn rpc_get_signatures_for_address(rpc_client: &RpcClient) {
-    let address = Pubkey::from_str("SCbotdTZN5Vu9h4PgSAFoJozrALn2t5qMVdjyBuqu2c").unwrap();
-
-    let config = GetConfirmedSignaturesForAddress2Config {
-        before: None,
-        until: None,
-        limit: Some(42),
-        commitment: Some(CommitmentConfig::confirmed()),
-    };
-
-    let signatures = rpc_client
-        .get_signatures_for_address_with_config(&address, config)
-        .await
-        .unwrap();
-
-    // 42
-    info!("Signatures: {:?}", signatures.len());
-}
-
-
-async fn websocket_account_subscribe(
-    rpc_url: Url
-)  {
-
-    let sysvar_subscribe =
-        json!({
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "accountSubscribe",
-            "params": [
-                "SysvarC1ock11111111111111111111111111111111"
-            ]
-        });
-
-    let mut ws1 = StableWebSocket::new_with_timeout(
-        rpc_url,
-        sysvar_subscribe.clone(),
-        Duration::from_secs(3),
-    )
-        .await
-        .unwrap();
-
-    let mut channel = ws1.subscribe_message_channel();
-
-    let mut count = 0;
-    while let Ok(msg) = channel.recv().await {
-        if let WsMessage::Text(payload) = msg {
-            info!("SysvarC1ock: {:?}", payload);
-            count += 1;
-        }
-        if count > 3 {
-            break;
-        }
-    }
 }
 
 
@@ -295,56 +130,59 @@ async fn websocket_source(
 }
 
 
-pub fn all_accounts() -> SubscribeRequest {
-    let mut accounts_subs = HashMap::new();
-    accounts_subs.insert(
+// note: this might fail if the yellowstone plugin does not allow "any broadcast filter"
+fn start_geyser_slots_task(config: GrpcSourceConfig,
+                           mpsc_downstream: tokio::sync::mpsc::Sender<Slot>,
+) {
+    let green_stream = create_geyser_reconnecting_stream(
+        config.clone(),
+        slots(),
+    );
+
+    tokio::spawn(async move {
+        let mut green_stream = pin!(green_stream);
+        while let Some(message) = green_stream.next().await {
+            match message {
+                Message::GeyserSubscribeUpdate(subscriber_update) => {
+                    match subscriber_update.update_oneof {
+                        Some(UpdateOneof::Slot(slot_info)) => {
+                            info!("Slot from geyser: {:?}", slot_info.slot);
+                            match mpsc_downstream.send(slot_info.slot).await {
+                                Ok(_) => {}
+                                Err(_) => return
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+        }
+    });
+}
+
+
+
+pub fn slots() -> SubscribeRequest {
+    let mut slot_subs = HashMap::new();
+    slot_subs.insert(
         "client".to_string(),
-        SubscribeRequestFilterAccounts {
-            account: vec![],
-            owner: vec![],
-            filters: vec![],
+        SubscribeRequestFilterSlots {
+            filter_by_commitment: Some(true),
         },
     );
 
-
     SubscribeRequest {
-        slots: Default::default(),
-        accounts: accounts_subs,
+        slots: slot_subs,
+        accounts: HashMap::new(),
         transactions: HashMap::new(),
         entry: Default::default(),
         blocks: Default::default(),
         blocks_meta: HashMap::new(),
-        commitment: None,
+        commitment: Some(yellowstone_grpc_proto::prelude::CommitmentLevel::Processed as i32),
         accounts_data_slice: Default::default(),
         ping: None,
     }
 }
 
-
-
-pub fn orca_token_account() -> SubscribeRequest {
-    let mut accounts_subs = HashMap::new();
-    accounts_subs.insert(
-        "client".to_string(),
-        SubscribeRequestFilterAccounts {
-            account: vec!["2WLWEuKDgkDUccTpbwYp1GToYktiSB1cXvreHUwiSUVP".to_string()],
-            owner: vec![],
-            filters: vec![],
-        },
-    );
-
-
-    SubscribeRequest {
-        slots: Default::default(),
-        accounts: accounts_subs,
-        transactions: HashMap::new(),
-        entry: Default::default(),
-        blocks: Default::default(),
-        blocks_meta: HashMap::new(),
-        commitment: None,
-        accounts_data_slice: Default::default(),
-        ping: None,
-    }
-}
-
-
+x
